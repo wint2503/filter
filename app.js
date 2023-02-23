7983eec7c239f13b81f4a4c5a2ed70d87edc66dd
Wint Thanda Moe <wintthandamoe@gmail.com>
2023-02-23 07:33:16 +0630

    day4

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ var getinput = document.getElementById('search');
 var getul = document.getElementById('members');
 var getli = getul.getElementsByTagName('li');
 // console.log(getli);
+var getsortbtn = document.getElementById('sort');
 
+// Sorting Method1
+getsortbtn.addEventListener('click', getsortazm1);
+
+function getsortazm1(){
+    console.log('i am working');
+}
+
+// Input Filter
 getinput.addEventListener('keyup', filter);
 
 function filter(){
@@ -11,7 +20,15 @@ function filter(){
 
     for(var x = 0 ; x < getli.length; x++){
         // console.log(getli[x].querySelector('a').innerText.toLowerCase());
-        console.log(getli[x].getElementsByTagName('a')[0].textContent.toLowerCase());
+        // console.log(getli[x].getElementsByTagName('a')[0].textContent.toLowerCase());
+
+        var getavalue = getli[x].querySelector('a').innerText.toLowerCase();
+
+        if(getavalue.indexOf(inputfilter) > -1){
+            getli[x].style.display = '';
+        }else{
+            getli[x].style.display = 'none';
+        }
     }
 }
 
