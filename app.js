6b6c11051b5a93122bce1f54d2e5bf141ab95990
Wint Thanda Moe <wintthandamoe@gmail.com>
2023-02-24 07:36:48 +0630

    sorting

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 var getinput = document.getElementById('search');
 var getul = document.getElementById('members');
 var getli = getul.getElementsByTagName('li');
-// console.log(getli);
 var getsortbtn = document.getElementById('sort');
 
 // Sorting Method1
+// li -> text -> insert to array -> sort -> make ul blank -> recrete newli -> insert newli to ul
 getsortbtn.addEventListener('click', getsortazm1);
 
 function getsortazm1(){
-    console.log('i am working');
-}
+    var lis = [];
+    for(var i = 0; i < getli.length; i++){
+        // console.log(getli[i].textContent);
+        lis.push(getli[i].textContent);
+    }
+    // console.log(lis.sort()); 
+    // console.log(lis.sort().reverse());
 
+    var azlis = lis.sort();
+    getul.innerHTML = '';
+
+    azlis.forEach(function(azli){
+        const newli = document.createElement('li');
+        const newlink = document.createElement('a');
+        newlink.href = `javascript:void(0);`;
+        newlink.appendChild(document.createTextNode(azli));
+        newli.appendChild(newlink);
+        getul.appendChild(newli);
+        // console.log(newli);
+    })
+    
+    }
+    
 // Input Filter
+// keyup -> tolowercase -> getvalue -> striped -> input in style
 getinput.addEventListener('keyup', filter);
 
 function filter(){
