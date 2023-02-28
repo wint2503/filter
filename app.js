cf8a8cf368ed8bf91ce5c7ed987285cafad1cb53
Wint Thanda Moe <wintthandamoe@gmail.com>
2023-02-28 07:34:07 +0630

    finish project

--- app.js
+++ app.js
@@ -17,11 +17,8 @@ function filter(){
     // console.log(inputfilter);
 
     for(var x = 0 ; x < getli.length; x++){
-        // console.log(getli[x].querySelector('a').innerText.toLowerCase());
         // console.log(getli[x].getElementsByTagName('a')[0].textContent.toLowerCase());
-
         var getavalue = getli[x].querySelector('a').innerText.toLowerCase();
-
         if(getavalue.indexOf(inputfilter) > -1){
             getli[x].style.display = '';
         }else{
@@ -29,23 +26,15 @@ function filter(){
         }
     }
 }
+// ---------------------------------
 
-
-
-// create array -> li -> text -> insert to array -> sort -> make ul blank -> create newli -> insert newli to ul
 getsortazm1btn.addEventListener('click', sortazm1);
 getsortzam1btn.addEventListener('click', sortzam1);
 getsortazm2btn.addEventListener('click', sortazm2);
-
-// Method2
-function sortazm2(){
-    console.log('i am working');
-
-    var shouldswitch = true;
-    var switching = true;
-}
+getsortzam2btn.addEventListener('click', sortzam2);
 
 // Method1
+// create array -> li -> text -> insert to array -> sort -> make ul blank -> create newli -> insert newli to ul
 function sortazm1(){
     var lis = [];
     for(var i = 0; i < getli.length; i++){
@@ -91,5 +80,67 @@ function sortzam1(){
     })
 }
     
+// Method2
+// switching -> switching=false -> shouldswitch=false -> for loop(compare) -> shouldswitch=true  -> getli(changeplace) up&down -> switching=true
+function sortazm2(){
+    var shouldswitch = true; // asking should change places?
+    var switching = true;  // changing place
 
+    // can loop limitless so we use
+    do{
+        switching = false;
+
+        var i;
+        // we sub 1 from getli.length bez can occur error in the end
+        for(i = 0; i < getli.length-1; i++ ){
+            shouldswitch = false;
+            // we compare existing index and next index number
+            if(getli[i].textContent.toLowerCase() > getli[i+1].textContent.toLowerCase()){
+            shouldswitch = true;
+            break;
+            }
+        }
+
+        if(shouldswitch){
+            // parentElement.insertBefore(new,existing), parentNode.insertBefore(new,existing)
+            getli[i].parentElement.insertBefore(getli[i+1],getli[i]);
+            switching = true;
+        }
+    }while(switching);
+}
+
+
+function sortzam2(){
+    var shouldswitch = true;
+    var switching = true;
+
+    do{
+        switching = false;
+        var i;
+        for(i = 0; i < getli.length-1; i++){
+            shouldswitch = false;
+           if(getli[i].textContent.toLowerCase() < getli[i+1].textContent.toLowerCase()){
+                shouldswitch = true;
+                break;
+           }
+        }
+
+        if(shouldswitch){
+            getli[i].parentNode.insertBefore(getli[i+1],getli[i]);
+            switching = true;
+        }
+    }while(switching);
+}
+// firstly subtract value from array
+// console.log(getli[0].textContent.toLowerCase());
+// console.log(getli[1].textContent.toLowerCase());
+
+//                 // Wint Thanda Moe         Aye Thandar Myint
+// if(getli[0].textContent.toLowerCase() > getli[1].textContent.toLowerCase()){
+//     console.log('true'); // W > A
+// }else if(getli[0].textContent.toLowerCase() === getli[1].textContent.toLowerCase()){
+//     console.log('equal');
+// }else{
+//     console.log('false');
+// }
 
